6a37d72955849a5b82d55bde9f9b14b225eb46dc
elizah <blancoelizah@gmail.com>
2025-11-26 11:51:30 +0800

    added codev

---
+++ src/events/CodevEVHandler.java
@@ -0,0 +1,24 @@
+package events;
+
+import layout.Card;
+import layout.design.DesignQuiz;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class CodevEVHandler extends KeyAdapter {
+
+    private DesignQuiz devs = new DesignQuiz();
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        if (e.getKeyCode() == KeyEvent.VK_C) {
+            System.out.println("Ctrl+C pressed: Activating CoDev...");
+            devs.useCoDev();
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            System.out.println("Ctrl+S pressed: Activating CoDev...");
+        }
+    }
+}
--- src/layout/constants/CustomButton.java
+++ src/layout/constants/CustomButton.java
@@ -37,6 +37,8 @@ public class CustomButton extends JButton {
     @Override
     protected void paintBorder(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         g2.setColor(getBackground().darker());
