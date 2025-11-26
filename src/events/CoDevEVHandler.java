62c8d682a7e9d88dfaba707da7e6a45360dfcaa7
ausdotsn50 <ausdotsn50@gmail.com>
2025-11-26 22:36:08 +0800

    first polish for presentation

--- src/events/CoDevEVHandler.java
+++ src/events/CoDevEVHandler.java
@@ -1,24 +1,20 @@
 package events;
 
-import layout.Card;
 import layout.design.DesignQuiz;
 import ud_interfaces.Play;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class CodevEVHandler extends KeyAdapter {
-    public static Play playScreen; 
-
-    private DesignQuiz devs = new DesignQuiz();
+public class CoDevEVHandler extends KeyAdapter {
     @Override
     public void keyPressed(KeyEvent e) {
-
         if (e.getKeyCode() == KeyEvent.VK_C) {
-            
-            playScreen.copy--;
-            System.out.println("Ctrl+C pressed: Activating CoDev...");
-            devs.useCoDev();
+            if(Play.copy >0) {
+                DesignQuiz.useCoDev();
+                Play.copy -=1;
+                System.out.println("Ctrl+C pressed: Activating CoDev...");
+            }
         }
 
         if (e.getKeyCode() == KeyEvent.VK_S) {
