a28cf7d0b26b931e1a2ac5bacc9fc660e9431443
ausdotsn50 <ausdotsn50@gmail.com>
2025-11-28 12:35:04 +0800

    jar with splash config

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import layout.Card;
+import misc.UDSplashScreen;
 
 public class Main {
     public static void main(String[] args) {
-        // UDSplashScreen udSplashScreen = new UDSplashScreen();
+        UDSplashScreen udSplashScreen = new UDSplashScreen();
 
         Card app = new Card();
         app.setVisible(true);
